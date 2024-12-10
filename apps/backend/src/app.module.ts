cefbde2b9001dd0188179b322d9816d815d36c08
Quentin(윤관) <quentin.yoon@goorm.io>
2024-12-10 06:01:50 +0000

    init: 초기 세팅(DB 연동)

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import Joi from 'joi';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			// validationSchema: Joi.object({
+			// 	BACKEND_PORT: Joi.number().default(4000),
+			// 	DATABASE_URL: Joi.string().uri().required(),
+			// }),
+		}),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				uri: configService.get<string>('DATABASE_URL'),
+			}),
+		}),
+		AuthModule,
+		UserModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+
+	// app.useGlobalPipes(
+	// 	new ValidationPipe({
+	// 		whitelist: true,
+	// 		forbidNonWhitelisted: true,
+	// 		transform: true,
+	// 	})
+	// );
+
+	app.enableCors({
+		origin: '*',
+		credentials: true,
+	});
+
+	const port = configService.get<number>('BACKEND_PORT');
+
+	await app.listen(port ?? 4000);
 }
+
 bootstrap();
