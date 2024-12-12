d3959aa889fa256c21e46cd8f9bb28cfca78db67
Quentin(윤관) <quentin.yoon@goorm.io>
2024-12-12 06:34:47 +0000

    feat(post): service, controller 반환타입, 예외처리, 반환값 공통 처리 추가 - service에 반환타입, 예외처리 추가 - controller에 반환타입, 반환값 공통으로 처리

--- apps/backend/src/post/post.controller.ts
+++ apps/backend/src/post/post.controller.ts
@@ -12,49 +12,77 @@ import { PostService } from './post.service';
 import { CreatePostDto } from './DTO/create-post.dto';
 import { SearchQueryDto } from './DTO/search-query.dto';
 import { UpdatePostDto } from './DTO/update-post.dto';
+import { Post as PostEntity } from './post.schema';
 
 @Controller('post')
 export class PostController {
 	constructor(private readonly postService: PostService) {}
 
 	@Post('new')
-	async createPost(@Body() createPostData: CreatePostDto) {
+	async createPost(
+		@Body() createPostData: CreatePostDto
+	): Promise<{ success: boolean; message: string; data: PostEntity }> {
 		const newPost = await this.postService.createPost(createPostData);
 
-		return { message: 'Created post successfully', newPost };
+		return {
+			success: true,
+			message: 'Created post successfully',
+			data: newPost,
+		};
 	}
 
 	@Get()
-	async searchPost(@Query() query: SearchQueryDto) {
+	async searchPost(
+		@Query() query: SearchQueryDto
+	): Promise<{ success: boolean; message: string; data: any }> {
 		const posts = await this.postService.searchPost(query);
 
-		return posts;
+		return {
+			success: true,
+			message: 'Fetched posts successfully',
+			data: posts,
+		};
 	}
 
 	@Get(':id')
-	async getPostById(@Param('id') id: string) {
+	async getPostById(
+		@Param('id') id: string
+	): Promise<{ success: boolean; message: string; data: PostEntity }> {
 		const post = await this.postService.getPostById(id);
 
-		return post;
+		return {
+			success: true,
+			message: 'Fetched post successfully',
+			data: post,
+		};
 	}
 
 	@Put(':id')
 	async updatePostById(
 		@Param('id') id: string,
 		@Body() updatePostDto: UpdatePostDto
-	) {
-		const updatePost = await this.postService.updatePostById(
+	): Promise<{ success: boolean; message: string; data: PostEntity }> {
+		const updatedPost = await this.postService.updatePostById(
 			id,
 			updatePostDto
 		);
 
-		return { message: 'Updated Post successfully', updatePost };
+		return {
+			success: true,
+			message: 'Updated post successfully',
+			data: updatedPost,
+		};
 	}
 
 	@Delete(':id')
-	async deletePostById(@Param('id') id: string) {
-		const deletePost = await this.postService.deletePostById(id);
+	async deletePostById(
+		@Param('id') id: string
+	): Promise<{ success: boolean; message: string }> {
+		await this.postService.deletePostById(id);
 
-		return { message: 'Deleted Post successfully', deletePost };
+		return {
+			success: true,
+			message: 'Deleted post successfully',
+		};
 	}
 }
--- apps/backend/src/post/post.service.ts
+++ apps/backend/src/post/post.service.ts
@@ -3,51 +3,57 @@ import { PostRepository } from './post.repository';
 import { CreatePostDto } from './DTO/create-post.dto';
 import { SearchQueryDto } from './DTO/search-query.dto';
 import { UpdatePostDto } from './DTO/update-post.dto';
+import { Post } from './post.schema';
 
 @Injectable()
 export class PostService {
 	constructor(private readonly postRepository: PostRepository) {}
 
-	async createPost(createPostData: CreatePostDto) {
+	async createPost(createPostData: CreatePostDto): Promise<Post> {
 		const newPost = await this.postRepository.createPost(createPostData);
 
 		return newPost;
 	}
 
-	async searchPost(query: SearchQueryDto) {
+	async searchPost(
+		query: SearchQueryDto
+	): Promise<{ postInfos: Post[]; totalCount: number; totalPage: number }> {
 		const posts = await this.postRepository.searchPost(query);
 
 		return posts;
 	}
 
-	async getPostById(id: string) {
+	async getPostById(id: string): Promise<Post> {
 		const post = await this.postRepository.getPostById(id);
 
 		if (!post) {
-			throw new NotFoundException(`Post with Id ${post} not found`);
+			throw new NotFoundException(`Post with Id ${id} not found`);
 		}
 
 		return post;
 	}
 
-	async updatePostById(id: string, updatePostDto: UpdatePostDto) {
+	async updatePostById(
+		id: string,
+		updatePostDto: UpdatePostDto
+	): Promise<Post> {
 		const updatePost = await this.postRepository.updatePostById(
 			id,
 			updatePostDto
 		);
 
 		if (!updatePost) {
-			throw new NotFoundException(`Post with ID ${updatePost} not found`);
+			throw new NotFoundException(`Post with ID ${id} not found`);
 		}
 
 		return updatePost;
 	}
 
-	async deletePostById(id: string) {
+	async deletePostById(id: string): Promise<{ deletedCount: number }> {
 		const deletePost = await this.postRepository.deletePostById(id);
 
 		if (!deletePost) {
-			throw new NotFoundException(`Post with ID ${deletePost} not found`);
+			throw new NotFoundException(`Post with ID ${id} not found`);
 		}
 
 		return deletePost;
